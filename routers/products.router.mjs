0ddf5aee3d10f4678ebfe4beabf6fc34a807e54d
minhajzein <minhajzein@gmail.com>
2025-07-31 20:40:38 +0530

    new arrival products api completed

--- routers/products.router.mjs
+++ routers/products.router.mjs
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getMostWantedProducts } from "../controllers/products.controller.mjs";
+import { getMostWantedProducts, getNewArrivalProducts } from "../controllers/products.controller.mjs";
 
 const router = Router()
 
 router.get('/most-wanted', getMostWantedProducts)
+router.get('/new-arrivals', getNewArrivalProducts)
+
 
 export default router
